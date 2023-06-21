c921eff2f1c3442b4db3c945622cfe49b03472e9
csAfter40 <csafter40@gmail.com>
2023-06-22 02:03:38 +0300

    add jump jump to projects

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,7 @@
 import wordleImg from "./images/projectImages/wordle.png";
-import walletImg from "./images/projectImages/wallet.png"
-
+import walletImg from "./images/projectImages/wallet.png";
+import jumpImg from "./images/projectImages/jump.png";
+ 
 const projects = [
     {
         id: 1,
@@ -21,6 +22,16 @@ const projects = [
         year: 2023,
         liveUrl: "https://main--super-florentine-6222ff.netlify.app/",
         codeUrl: "https://github.com/csAfter40/wordle-clone",
+    },
+    {
+        id: 3,
+        title: "Jump Jump",
+        image: jumpImg,
+        description: "Jump Jump is a vertical endless platformer game written in Python using the Pygame module. The aim is to achieve the highest score by reaching higher platforms and collecting coins.",
+        techStack: ["python", "pygame"],
+        year: 2021,
+        liveUrl: null,
+        codeUrl: "https://github.com/csAfter40/jump-jump",
     },
 ]
 
